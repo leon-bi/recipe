1c2e92cd073080c20f21159c14819452c6bc7bec
leon-bi <leonbichanga@gmail.com>
2024-03-05 08:25:27 +0300

    Create api class to communicate with the web serverand our app

---
+++ lib/models/recipe.api.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:recipe/models/recipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https(
+        'yummly2.p.rapidapi.com', '/feeds/list', {'limit': '24', 'start': '0'});
+  final response = await http.get(uri,headers: {		'X-RapidAPI-Key': '41cd718136msh55e37f744b1a5a0p1ca7d2jsn86aa403284eb',
+		'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'});
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']){
+      // get the data of contents and details key
+      _temp.add(i['content']['details']);
+// return recipeSnapshotfucnction that takes in _temp list
+      return Recipe.recipesFromSnapshot(_temp);
+    }
+  }
+}
+
